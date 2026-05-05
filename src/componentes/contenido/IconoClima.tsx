f7b3e6dfef259a114c8f7ad5e42e35365af22105
evelyn villarreal <evelyn.villarreal245@gmail.com>
2026-05-05 01:44:47 -0300

    agregue los iconos principales mediante la libreria lucede

--- src/componentes/contenido/IconoClima.tsx
+++ src/componentes/contenido/IconoClima.tsx
@@ -1,28 +1,22 @@
-import { View, Text, StyleSheet } from "react-native";
-
-export default function IconoClima({ condition }: { condition: string }) {
-  let icon = "☀️";
-
-  if (condition === "rain") icon = "🌧️";
-  if (condition === "cloudy") icon = "☁️";
-
- return (
-    <View
-      testID={`icon-weather-${condition}`}
-      accessibilityRole="image"
-      style={styles.container}
-    >
-      <Text style={styles.icon}>{icon}</Text>
-    </View>
-  );
-}
+import { Sun, CloudRain, Cloud } from "lucide-react-native";
+import { View, StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
     marginVertical: 20,
   },
-  icon: {
-    fontSize: 90,
-  },
 });
+
+export default function IconoClima({ condition }: { condition: string }) {
+  const size = 90;
+  const strokeWidth = 1.5;
+
+  return (
+    <View testID={`icon-weather-${condition}`} accessibilityRole="image" style={styles.container}>
+      {condition === "sunny" && <Sun size={size} strokeWidth={strokeWidth} color="#000" />}
+      {condition === "rain" && <CloudRain size={size} strokeWidth={strokeWidth} color="#000" />}
+      {condition === "cloudy" && <Cloud size={size} strokeWidth={strokeWidth} color="#000" />}
+    </View>
+  );
+}
